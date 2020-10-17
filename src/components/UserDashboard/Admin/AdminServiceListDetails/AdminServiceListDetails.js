129472380cd31aa4cae281fa85111c0660fe5bc7
AmjadRabby <glamarrabby@gmail.com>
2020-10-17 23:12:48 +0600

    done

--- src/components/UserDashboard/Admin/AdminServiceListDetails/AdminServiceListDetails.js
+++ src/components/UserDashboard/Admin/AdminServiceListDetails/AdminServiceListDetails.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 
 const AdminServiceListDetails = ({service}) => {
+
+    const [pending, setPending]=useState(service.status.toLowerCase()=='pending')
+
+    const statusHandler=(e)=>{
+
+    }
     return (
         
         <tr style={{ fontWeight:'400',}}>
@@ -9,6 +16,25 @@ const AdminServiceListDetails = ({service}) => {
             <td>{service.title}</td>
             <td>{service.description}</td>
             <td style={{color:'#FF4545'}}>{service.status}</td>
+            <td>
+            {
+                pending &&
+                 <Form.Control as="select" className='text-danger' onChange={statusHandler}>
+                    <option  selected style={{color:'#FF4545'}}>Pending</option>
+                    <option style={{color:'#009444'}}>Done</option>
+                </Form.Control>
+            }
+            {
+                !pending &&
+                <Form.Control className='text-success' as="select" onChange={statusHandler}>
+                    <option style={{color:'#FF4545'}}>Pending</option>
+                    <option selected style={{color:'#009444'}}>Done</option>
+                </Form.Control>
+            }
+        
+
+            </td>
+            
         </tr>
     );
 };
